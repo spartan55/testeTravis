286b3b6715c1161eaeb34a00304e5d2efbd0ff55
spartan55 <joeu123@gmail.com>
2016-09-02 02:17:14 -0300

    Add files via upload

---
+++ src/cc/engeld/Teste.java
@@ -0,0 +1,19 @@
+package cc.engeld;
+
+public class Teste {
+
+    public static void main(String Args[]) {
+        Gerador g = new Gerador();
+        Integer[] numeros = g.gerarNumerosAleatorios(10, 100);
+        
+        for (Integer i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i] + "\n");
+        }
+        SortElements testeOrdenacao = new Bubble();
+        testeOrdenacao.sortItems(numeros);
+        System.out.println("Algoritmo Ordenacao\n");
+        for (Integer i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i] + "\n");
+        }
+    }
+}
